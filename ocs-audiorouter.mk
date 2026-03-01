96cc3c7552fd274a4cabe87e335fa08088be8093
OilCanSteve <OilCanSteve@hotmail.com>
2026-03-01 18:05:26 +0000

    some adjustments

--- ocs-audiorouter.mk
+++ ocs-audiorouter.mk
@@ -3,7 +3,7 @@
 # audiorouter
 #
 ######################################
-AUDIOROUTER_VERSION = 4478ead1bb8114f41a61603f43a6147af368f8b3
+AUDIOROUTER_VERSION = e4995c2093662a6948b8475a015b9fa7437ab7b3
 AUDIOROUTER_SITE = https://github.com/OilCanSteve/ocs-audiorouter
 AUDIOROUTER_SITE_METHOD = git
 AUDIOROUTER_BUNDLES = ocs-audiorouter.lv2
